80eee55c08f363e64694e5de4560858eeee98025
zhanghongda <zhanghongda@baidu.com>
2019-10-17 11:06:48 +0800

    add common app wrapper

---
+++ xdot.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+#
+# Copyright 2008 Jose Fonseca
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys
+import gi
+gi.require_version('Gtk', '3.0')
+
+from gi.repository import Gtk
+
+import xdot
+
+
+class MyDotWindow(xdot.DotWindow):
+
+    def __init__(self):
+        xdot.DotWindow.__init__(self)
+        self.dotwidget.connect('clicked', self.on_url_clicked)
+
+    def on_url_clicked(self, widget, url, event):
+        dialog = Gtk.MessageDialog(
+            parent=self,
+            buttons=Gtk.ButtonsType.OK,
+            message_format="%s clicked" % url)
+        dialog.connect('response', lambda dialog, response: dialog.destroy())
+        dialog.run()
+        return True
+
+
+def main():
+    window = MyDotWindow()
+    if len(sys.argv) >= 2:
+        fname = sys.argv[1]
+        with open(fname, 'rb') as f:
+            b = f.read()
+        f.close()
+        window.set_dotcode(b)
+    window.connect('delete-event', Gtk.main_quit)
+    Gtk.main()
+
+
+if __name__ == '__main__':
+    main()
